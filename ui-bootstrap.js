64306dab35fb62fbb28b3c04f6229356f102b0bb
AParks <parksa@seas.upenn.edu>
2017-08-02 11:27:16 +1000

    feat(typeahead): handles min-length of 0
    keep typeahead popup open even when all input has been deleted. Still uses a default value of 1 for backward compatibility (when min-length is falsy but not === 0).
    
    see [#3600](https://github.com/angular-ui/bootstrap/pull/3600) and [commit](https://github.com/angular-ui/bootstrap/commit/a5a25141fc88e642b84e135796daaf77dc653273)

--- ui-bootstrap.js
+++ ui-bootstrap.js
@@ -3554,9 +3554,9 @@ angular.module('ui.bootstrap.typeahead', ['ui.bootstrap.position', 'ui.bootstrap
       //SUPPORTED ATTRIBUTES (OPTIONS)
 
       //minimal no of characters that needs to be entered before typeahead kicks-in
-      var minSearch = originalScope.$eval(attrs.typeaheadMinLength);
-      if (!minSearch && minSearch !== 0) {
-        minSearch = 1;
+      var minLength = originalScope.$eval(attrs.typeaheadMinLength);
+      if (!minLength && minLength !== 0) {
+        minLength = 1;
       }
 
       //minimal wait time after last character typed before typehead kicks-in
@@ -3709,7 +3709,7 @@ angular.module('ui.bootstrap.typeahead', ['ui.bootstrap.position', 'ui.bootstrap
 
         hasFocus = true;
 
-        if (inputValue && inputValue.length >= minSearch) {
+        if (minLength === 0 || inputValue && inputValue.length >= minLength) {
           if (waitTime > 0) {
             cancelPreviousTimeout();
             scheduleSearchWithTimeout(inputValue);
@@ -3824,7 +3824,7 @@ angular.module('ui.bootstrap.typeahead', ['ui.bootstrap.position', 'ui.bootstrap
 
       element.bind('focus', function (evt) {
         hasFocus = true;
-        if (minSearch === 0 && !modelCtrl.$viewValue) {
+        if (minLength === 0 && !modelCtrl.$viewValue) {
           $timeout(function() {
             getMatchesAsync(modelCtrl.$viewValue, evt);
           }, 0);
